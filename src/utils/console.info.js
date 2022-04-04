c3e6b82df0d3e8fb9eb7385613086057fc517a13
xly <32870419@qq.com>
2022-04-04 20:15:36 +0800

    feat: 杂

---
+++ src/utils/console.info.js
@@ -0,0 +1,9 @@
+/**
+ * 打印项目信息
+ */
+console.log(
+  process.env.VUE_APP_PACKAGE_NAME,
+  process.env.NODE_ENV,
+  process.env.VUE_APP_PACKAGE_VERSION,
+  process.env.VUE_APP_PACKAGE_DESCRIPTION,
+);
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ const config = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     open: true,
